720fd2cbeb82afc3fdbef313f8e1675276578e1b
Sylvain Cau <10268458+sylc@users.noreply.github.com>
2023-02-01 21:42:25 +0800

    fix: Correct minVersion required (#15)
    * fix minVersion required
    
    * x

--- src/utils/versions.test.ts
+++ src/utils/versions.test.ts
@@ -2,7 +2,11 @@ import { assert } from "../../deps_test.ts";
 import { assertMinVersion } from "./versions.ts";
 
 Deno.test("assertMinVersion", () => {
+  // should be good
   assert(assertMinVersion("1.1.5", "1.0.1"));
   assert(assertMinVersion("2.0.5", "1.0.1"));
+  assert(assertMinVersion("1.30.0", "1.29.1"));
+  // not good
   assert(!assertMinVersion("1.0.5", "2.0.0"));
+  assert(!assertMinVersion("1.27.5", "1.30.0"));
 });
